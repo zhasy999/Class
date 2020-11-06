8ef842fecddd04e2a2ca9962745cab28cace390f
zhasy999 <zhasy99@mail.ru>
2020-11-06 22:07:41 +0600

    hys

--- src/main/java/com/example/demo/ClassApplication.java
+++ src/main/java/com/example/demo/ClassApplication.java
@@ -2,8 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrix
+@EnableHystrixDashboard
 public class ClassApplication {
 
 	public static void main(String[] args) {
